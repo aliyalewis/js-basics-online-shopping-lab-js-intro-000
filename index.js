8c7ca303b68570e0a30612d769ead64ed9326964
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-23 03:03:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(name) {
+  var price = Math.random()*100
+  Math.floor(price)
+
+  cart.push({itemName: name, itemPrice: price})
+  return `${name} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
-}
+  if(cart.length === 0){
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${name} at ${}`
+  }
+
+  }
 
 function total() {
   // write your code here
